e57f9992175396b2fd75943136e6ad76b9c55a6e
Leonardo Lopez <leodanlors1993@gmail.com>
2023-03-30 01:09:02 -0600

    landing section

---
+++ src/components/landing/Landing.vue
@@ -0,0 +1,19 @@
+<script setup>
+import PrincipalSection from './PrincipalSection.vue';
+</script>
+
+<template>
+  <div>
+    <principal-section/>
+  </div>
+</template>
+
+<style scoped>
+
+
+
+
+@media (min-width: 1024px) {
+  
+}
+</style>
---
+++ src/components/landing/PrincipalSection.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div class="principal-section">
+        <div class="content">
+            <img 
+                src="@/assets/PaqueteAtomic/Group4032.png" 
+                class="figure">
+            <p>Desarrolla todo tu POTENCIAL dentro del equipo ATOMICLABS</p>
+            <button>¡Quiero ser parte!</button>
+        </div>
+        <div>
+            <img src="@/assets/PaqueteAtomic/Group4013@2x.png">
+            <p>Quiero saber más</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+.principal-section{
+    background-image: url("@/assets/PaqueteAtomic/MaskGroup1@2x.png");
+    background-size:cover;
+}
+.content{
+    display: flex;
+    flex-wrap: wrap;
+}
+.content p {
+    width: 50vw;
+}
+.figure{
+    width:300px;
+}
+
+</style>
